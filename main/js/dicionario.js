9d9839ae2c1d9e02b822a818401350f4b0752c8a
kevin alexsander <kevin13348@gmail.com>
2021-10-25 16:36:43 -0300

    added a modal demonstration to alerts and dictionary

---
+++ main/js/dicionario.js
@@ -0,0 +1,4 @@
+const ldiag = {
+    name: 'LDIAG',
+    description: 'LDIAG (Lenovo Diagnostics - Diagnóstico Lenovo) é um software utilizado para testar os componentes internos de uma máquina, tais como: Processador, Memória RAM e HD/SSD.'
+}
