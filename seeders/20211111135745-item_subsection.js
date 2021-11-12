5522d90f884ef5f7c6ce188bbd29ae5a41c60eaa
kevin alexsander <kevin13348@gmail.com>
2021-11-12 16:21:21 -0300

    added dark mode and fixed HTML data from db

--- seeders/20211111135745-item_subsection.js
+++ seeders/20211111135745-item_subsection.js
@@ -39,6 +39,24 @@ module.exports = {
         description:'<p>Em alguns casos é <strong>necessário</strong> que sejam realizados <strong>reparos</strong> ou até mesmo a <strong>troca</strong> da <strong>Placa-Mãe</strong> (também conhecida como Main-Board ou Mother-Board). Nestes casos é possível que algumas <strong>informações</strong> sejam <strong>apagadas da BIOS</strong> ou estejam <strong>incorretas</strong>.</p><p>Esta seção compila as instruções para, principalmente, <strong>regravar</strong> tais informações.</p>',
         itemSectionId: 2,
       },
+      {
+        id: 7,
+        name: 'Usuário de Teste',
+        description:'<p>Em maquinas com o sistema <strong>Windows</strong>, em casos de <strong>reinstalação do Sistema Operacional</strong> ou <strong>substituição do HD/SSD</strong> é preciso que os testes sejam realizados em um <strong>Usuário de Testes</strong> que deve ser <strong>Excluído</strong> ao final do processo.</p>',
+        itemSectionId: 2,
+      },
+      {
+        id: 8,
+        name: 'Usuário do Cliente Bloqueado',
+        description:'<p>É comum que os usuários configurem uma <strong>senha</strong> em seus dispositivos. Alguns destes clientes optam por <strong>remover</strong> sua senha ou mandar um <strong>bilhete</strong> com a senha para auxiliar na assistencia técnica. Entretanto, quando isso <strong>não acontece</strong> é preciso seguir os procedimentos contidos nesta seção para que a realização dos testes seja <strong>possível</strong> .</p>',
+        itemSectionId: 2,
+      },
+      {
+        id: 9,
+        name: 'Outros',
+        description:'<p>Os procedimentos a seguir são <strong>singulares</strong>. Talvez isso aconteça por serem muito <strong>específicos</strong> ou por resolverem um problema do inicio ao fim. Sendo assim, o fato de não estarem contidos em  outras seções <strong>não</strong>  os tornam menos relevantes.</p>',
+        itemSectionId: 2,
+      },
     ], 
     {});
   },
--- seeders/20211111185207-item.js
+++ seeders/20211111185207-item.js
@@ -33,6 +33,130 @@ module.exports = {
         title: 'Ativação do Sistema',
         itemSubsectionId: 2
       },
+      {
+        id: 7,
+        title: 'Atualização de Drivers',
+        itemSubsectionId: 3
+      },
+      {
+        id: 8,
+        title: 'Atualização de Bios',
+        itemSubsectionId: 3
+      },
+      {
+        id: 9,
+        title: 'Teste de Microfone',
+        itemSubsectionId: 4
+      },
+      {
+        id: 10,
+        title: 'Teste de Tela',
+        itemSubsectionId: 4
+      },
+      {
+        id: 11,
+        title: 'Teste de audio / Vídeo',
+        itemSubsectionId: 4
+      },
+      {
+        id: 12,
+        title: 'Teste de HDMI',
+        itemSubsectionId: 4
+      },
+      {
+        id: 13,
+        title: 'Câmera',
+        itemSubsectionId: 4
+      },
+      {
+        id: 14,
+        title: 'Teste de Conexão Wi-Fi',
+        itemSubsectionId: 4
+      },
+      {
+        id: 15,
+        title: 'Ativação do PortCode',
+        itemSubsectionId: 4
+      },
+      {
+        id: 16,
+        title: 'Teste de USB',
+        itemSubsectionId: 5
+      },
+      {
+        id: 17,
+        title: 'Teste de Teclado',
+        itemSubsectionId: 5
+      },
+      {
+        id: 18,
+        title: 'Teste de Touchpad',
+        itemSubsectionId: 5
+      },
+
+      {
+        id: 19,
+        title: 'Consulta de DPK',
+        itemSubsectionId: 6
+      },
+      {
+        id: 20,
+        title: 'Gravação das Informaçõe da Placa-Mãe',
+        itemSubsectionId: 6
+      },
+      {
+        id: 21,
+        title: 'Inserção de DPK',
+        itemSubsectionId: 6
+      },
+      {
+        id: 22,
+        title: 'Inserção de BaseBoard',
+        itemSubsectionId: 6
+      },
+
+      {
+        id: 23,
+        title: 'Criar Usuário',
+        itemSubsectionId: 7
+      },
+      {
+        id: 24,
+        title: 'Excluir Usuário',
+        itemSubsectionId: 7
+      },
+
+      {
+        id: 25,
+        title: 'Quebra de Senha',
+        itemSubsectionId: 8
+      },
+      {
+        id: 26,
+        title: 'Desativar Administrador',
+        itemSubsectionId: 8
+      },
+      {
+        id: 27,
+        title: 'Entrar em Outro Domínio',
+        itemSubsectionId: 8
+      },
+
+      {
+        id: 28,
+        title: 'Teste LDIAG',
+        itemSubsectionId: 9
+      },
+      {
+        id: 29,
+        title: 'Desinstalação de Anti-Vírus',
+        itemSubsectionId: 9
+      },
+      {
+        id: 30,
+        title: 'Teclas de Atalho',
+        itemSubsectionId: 9
+      },
     ], 
     {});
   },
