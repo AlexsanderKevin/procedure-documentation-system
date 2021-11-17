153d7f4e3e71c13d7d3d396227e572e4202eabd6
Kevin <kevin13348@gmail.com>
2021-11-17 10:17:34 -0300

    new nav-bar

---
+++ public/js/nav.js
@@ -0,0 +1,36 @@
+const nav_links = document.querySelector('.nav_links')
+const burger = document.querySelector('.burger')
+//arrays
+const section_links = document.querySelectorAll('.section_links')
+const subsection_links = document.querySelectorAll('.subsection_links')
+
+for(let i = 0; i < section_links.length; i ++){
+    section_links[i].addEventListener('click', () => {
+    subsection_links[i].classList.toggle('show')
+    subsection_links[i].classList.toggle('while_hidden')
+})
+}
+
+burger.addEventListener('click', (e) => {
+    nav_links.classList.toggle('show')
+
+    for(var i = 0; i <subsection_links.length;i++){
+
+        subsection_links[i].classList.remove('show')
+        subsection_links[i].classList.add('while_hidden')
+    }
+
+})
+
+
+
+for(var i = 0; i < subsection_links.length; i++){
+    subsection_links[i].addEventListener('click', (e)=>{
+        nav_links.classList.toggle('show')
+
+
+        subsection_links[i].classList.toggle('show')
+        subsection_links[i].classList.toggle('while_hidden')
+        
+    })
+}
