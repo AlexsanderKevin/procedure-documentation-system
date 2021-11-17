f9619192cd0a029a782e04fce4944f69364d1943
Kevin <kevin13348@gmail.com>
2021-11-17 15:01:07 -0300

    added a hide and show property to the subsections

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,17 +1,17 @@
 const nav_links = document.querySelector('.nav_links')
-const burger = document.querySelector('.burger')
+const nav_burger = document.querySelector('#nav_burger')
 //arrays
 const section_links = document.querySelectorAll('.section_links')
 const subsection_links = document.querySelectorAll('.subsection_links')
 
 for(let i = 0; i < section_links.length; i ++){
     section_links[i].addEventListener('click', () => {
-    subsection_links[i].classList.toggle('show')
-    subsection_links[i].classList.toggle('while_hidden')
-})
+        subsection_links[i].classList.toggle('show')
+        subsection_links[i].classList.toggle('while_hidden')
+    })
 }
 
-burger.addEventListener('click', (e) => {
+nav_burger.addEventListener('click', (e) => {
     nav_links.classList.toggle('show')
 
     for(var i = 0; i <subsection_links.length;i++){
