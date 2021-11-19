31375c2bc24cf7c69440078c001949b3ff3f2d5c
Kevin <kevin13348@gmail.com>
2021-11-19 10:35:55 -0300

    added a item obs table

---
+++ database/tables/item_obs.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+
+const database = require('../db')
+const Item = require('./item')
+
+const ItemObs = database.define('item_obs', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+Item.hasMany(ItemObs, {constraint: true, foreingnKey: 'itemId'})
+
+ItemObs.belongsTo(Item, {constraint: true, foreingnKey: 'itemId'})
