109c604d86dc11488a14baf55e5f64ea3c9a1b69
Kevin <kevin13348@gmail.com>
2021-11-19 15:09:58 -0300

    added a item issues table to database

---
+++ database/tables/item_issues.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize')
+
+const database = require('../db')
+const Item = require('./item')
+
+const ItemIssue = database.define('item_issues', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+Item.hasMany(ItemIssue, {constraint: true, foreingnKey: 'itemId'})
+
+ItemIssue.belongsTo(Item, {constraint: true, foreingnKey: 'itemId'})
+
+module.exports = ItemIssue
