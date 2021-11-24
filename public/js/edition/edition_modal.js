90915792b402e1386a0a2b7b691dd9443b67a3f5
Kevin <kevin13348@gmail.com>
2021-11-24 13:31:30 -0300

    creating the prcedure content partial

--- public/js/edition/edition_modal.js
+++ public/js/edition/edition_modal.js
@@ -1,31 +1,3 @@
-function open_edition(start_edition, discard_edition, save_edition, edition_buttons){
-    const start = document.querySelectorAll(start_edition)
-    const close = document.querySelectorAll(discard_edition)
-    const save = document.querySelectorAll(save_edition)
-    const buttons = document.querySelectorAll(edition_buttons)
-
-    for(let i = 0; i < start.length; i++){
-        start[i].addEventListener('click', ()=>{
-            
-            for(let i = 0; i < buttons.length; i++){
-                buttons[i].classList.toggle('show_button')
-            }
-        })
-        close[i].addEventListener('click', ()=>{
-            for(let i = 0; i < buttons.length; i++){
-                buttons[i].classList.toggle('show_button')
-            }
-        })
-        save[i].addEventListener('click', ()=>{
-            for(let i = 0; i < buttons.length; i++){
-                buttons[i].classList.toggle('show_button')
-            }
-        })
-    }
-}
-
-open_edition('.start_edition', '.discard_button', '.save_button', '.edition_button')
-
 // getting the modal
 function form_modal(modal_id){
     const modal = document.querySelector(modal_id)
