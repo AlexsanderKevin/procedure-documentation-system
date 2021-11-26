c1da0fc72d956812f5231564c16940b445639103
Kevin <kevin13348@gmail.com>
2021-11-26 14:42:52 -0300

    added input lists to forms and more three tables to be avaliable to edition

---
+++ public/js/edition/edition_page.js
@@ -0,0 +1,29 @@
+const table = document.querySelector('#table')
+const operation = document.querySelector('#operation')
+const add_user = document.querySelector('#add_user_form')
+const add_section = document.querySelector('#add_section_form')
+const add_procedure = document.querySelector('#add_procedure_form')
+const add_department = document.querySelector('#add_department_form')
+const add_subsection = document.querySelector('#add_subsection_form')
+
+function show_form(target_value, targert_form, target_operation_value){
+    if(table.value == target_value && operation.value == target_operation_value){
+        targert_form.classList.add('show')
+    }else{
+        targert_form.classList.remove('show')
+    }
+}
+
+function define_operation(){
+    show_form('department', add_department, 'add')
+    show_form('subsection', add_subsection, 'add')
+    show_form('procedure', add_procedure, 'add')
+    show_form('section', add_section, 'add')
+    show_form('user', add_user, 'add')
+    
+}
+
+operation.addEventListener('change', ()=>{define_operation()})
+table.addEventListener('change', ()=>{define_operation()})
+
+console.log('serto')
