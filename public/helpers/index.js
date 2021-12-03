1b5eee25178fa0a6c76da5437e5043539066e909
Kevin <kevin13348@gmail.com>
2021-12-03 06:56:27 -0300

    ignore

---
+++ public/helpers/index.js
@@ -0,0 +1,5 @@
+const t_if = require('./t_if.js')
+
+module.exports = {
+    t_if: t_if,
+}
---
+++ public/helpers/t_if.js
@@ -0,0 +1,7 @@
+module.exports = function(value_1, value_2){
+    if (value_1 == value_2){
+        return true
+    }else{
+        return false
+    }
+}
