63572290aba8449c670e9860dfd1bc85d16ee7b6
Kevin <kevin13348@gmail.com>
2022-01-04 08:31:40 -0300

    update: refactored modal.js

--- public/js/main/modal.js
+++ public/js/main/modal.js
@@ -1,40 +1,21 @@
 // getting the modal
-const modal = document.querySelector('#modal')
-const modal_close_button = document.querySelector('.modalCloseButton')
-const modal_content = document.querySelector('.modalContent')
-const modal_title = document.querySelector('.modal_title')
+const MODAL = document.querySelector('#modal')
+const CLOSE_BUTTON = MODAL.querySelector('.modalCloseButton')
+const MODAL_CONTENT = MODAL.querySelector('.modalContent')
+const MODAL_TITLE = MODAL.querySelector('.modal_title')
 
-modal_close_button.addEventListener('click', () => {
-    modal.classList.remove('show')
+CLOSE_BUTTON.addEventListener('click', () => {
+    MODAL.classList.remove('show')
 })
-function dynamic_modal(target_component, title_component, content_component){
-    // arrays
-    const target = document.querySelectorAll(target_component)
-    const title = document.querySelectorAll(title_component)
-    const content = document.querySelectorAll(content_component)
-    
-    for(let i = 0; i < target.length; i++){
-        target[i].addEventListener('click', ()=>{
-            modal.classList.toggle('show')
-            modal_title.innerText = title[i].innerText
-            // modal_content.innerHTML = content[i].innerHTML
-        })
-    }
-}
-
-// subsection description modal
-// dynamic_modal('.subsection_about_button', '.subsection_name', '.subsection_description')
-
-function static_modal(target_component, title, content){
+const static_modal = (target_component, title, content) => {
     const target = document.querySelectorAll(target_component)
 
-    for(let i = 0; i < target.length; i++){
-        target[i].addEventListener('click', ()=>{
-            modal.classList.toggle('show')
-            modal_title.innerText = title
-            modal_content.innerText = content
-        })
-    }
+    const TARGETS = document.querySelectorAll(target_component)
+    TARGETS.forEach(target => target.addEventListener('click', ()=>{
+        MODAL.classList.toggle('show')
+        MODAL_CONTENT.innerText = content
+        MODAL_TITLE.innerText = title
+    }))
 }
 // empty item
 static_modal('.emptyItemList_title', 'Item vazio!', 'Este item não possui conteúdo algum para ser visualizado no momento.')
