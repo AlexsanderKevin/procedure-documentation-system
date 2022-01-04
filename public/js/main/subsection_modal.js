028af25370dc3162768dcaccf93c86001f5d1837
Kevin <kevin13348@gmail.com>
2022-01-04 09:39:00 -0300

    update: refactored file subsection_modal.js

--- public/js/main/subsection_modal.js
+++ public/js/main/subsection_modal.js
@@ -3,13 +3,11 @@ const MODAL_FRAMES = document.querySelectorAll('.description_modal_container')
 const ABOUT_BUTTONS = document.querySelectorAll('.subsection_about_button')
 const CLOSE_BUTTONS = document.querySelectorAll('.close_description_modal')
 
-for(let i = 0; i < ABOUT_BUTTONS.length; i++){
+ABOUT_BUTTONS.forEach((button, index)=> button.addEventListener('click', ()=>{
+    MODAL_CONTAINERS[index].classList.add('show')
+    MODAL_FRAMES[index].classList.add('show')
+}))
 
-    ABOUT_BUTTONS[i].addEventListener('click', ()=>{
-        MODAL_CONTAINERS[i].classList.add('show')
-        MODAL_FRAMES[i].classList.add('show')
-    })
-}
 CLOSE_BUTTONS.forEach(button => {
 
     button.addEventListener('click', ()=>{
