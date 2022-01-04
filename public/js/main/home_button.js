66bdafdf284540de767a3dc1b4aa8c4cc1331648
Kevin <kevin13348@gmail.com>
2022-01-04 08:30:58 -0300

    update: refactored home_button.js

--- public/js/main/home_button.js
+++ public/js/main/home_button.js
@@ -1,20 +1,14 @@
-import { get } from "../../lib/main.js"
+const home_close = (content_component, show_class) => {
 
-function home_close(content_component, show_class){
-    const button = document.querySelector('#home_button')
-    const content = get(content_component)
+    const BUTTON = document.querySelector('#home_button')
+    const CONTENTS = document.querySelectorAll(content_component)
 
-    button.addEventListener('click', (e)=>{
-        for(let i = 0; i < content.length; i++){
-            content[i].classList.remove(show_class)
-        }
+    BUTTON.addEventListener('click', ()=>{
+        CONTENTS.forEach(content => content.classList.remove(show_class))
     })
-    
 }
-
 // home page
 home_close('.item_subsection_content', 'show_items') // close subsections
-
 // procedure page
 home_close('.item_content_text', 'show_text') // close the content sections
 home_close('.content_text', 'show_text') // close the single contents
