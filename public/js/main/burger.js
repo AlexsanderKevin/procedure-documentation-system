d2ab35d0bd8359dbe78277ce0add843738cd1a10
Kevin <kevin13348@gmail.com>
2022-01-04 08:07:41 -0300

    update: refactored the file burger.js

--- public/js/main/burger.js
+++ public/js/main/burger.js
@@ -2,51 +2,40 @@ import {get} from '../../lib/main.js'
 
 // dynamic burger for dynamic content
 function burger(button_component, content_component, show_class, subcontent_component, hidden_class){
-    const button = document.querySelectorAll(button_component)
-    const content = document.querySelectorAll(content_component)
-    const subcontent = document.querySelectorAll(subcontent_component)
+    const BUTTONS = document.querySelectorAll(button_component)
+    const CONTENTS = document.querySelectorAll(content_component)
 
-    for(let i = 0; i < button.length; i++){
-        button[i].addEventListener('click', ()=>{
+    BUTTONS.forEach(button => {
+        button.addEventListener('click', ()=>{
 
-            // toggle the show class in the content component
-            for(let i = 0; i < content.length; i++){
-                content[i].classList.toggle(show_class)
-            }
+            CONTENTS.forEach(item => {
+                item.classList.toggle(show_class)
 
-            // add the hidden class when click
-            for(let i = 0; i < subcontent.length; i++){
-                subcontent[i].classList.remove(show_class)
-                subcontent[i].classList.add(hidden_class)
-            }
+                const SUBCONTENTS = item.querySelectorAll(subcontent_component)
+                SUBCONTENTS.forEach(subcontent => {
+                    subcontent.classList.remove(show_class)
+                    subcontent.classList.add(hidden_class)
+                })
+            })
         })
-    }
+    })
 }
-
 // main nav burger
 burger('#nav_burger', '.nav_links', 'show', '.subsection_links', 'while_hidden')
-// section menu 
-burger('.settings_button', '.settings_links_container', 'show')
 
 // dynamic burger to static content
 function ds_burger(button_component, content_component, show_class){
-    const button = get(button_component)
-    const content = get(content_component)
 
-    for(let i = 0; i < button.length; i++){
-        button[i].addEventListener('click', ()=>{
-            content[i].classList.toggle(show_class)
-        })
-    }
+    const BUTTONS = document.querySelectorAll(button_component)
+    BUTTONS.forEach(button => {
+        const PARENT = button.parentElement.parentElement
+        const CONTENT = PARENT.querySelector(content_component)
+
+        button.addEventListener('click', ()=>{CONTENT.classList.toggle(show_class)})
+    })
 }
-
 // item subsection burger
 ds_burger('.subsection_burger', '.item_subsection_content', 'show_items')
 // procedure page
 ds_burger('.page_content_burger', '.item_content_text', 'show_text') // procedure sections
 ds_burger('.single_content_burger', '.content_text', 'show_text') // single content
-
-function somar(a, b, c){
-    alert(a + b + c)
-}
-
