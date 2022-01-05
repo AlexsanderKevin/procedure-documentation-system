9bf7888db5d5fa797ffff315c557d9a3f4efff8c
Kevin <kevin13348@gmail.com>
2022-01-05 07:49:07 -0300

    fixed issue: console error in empty home pages with modal.js

--- public/js/main/modal.js
+++ public/js/main/modal.js
@@ -1,21 +1,24 @@
 // getting the modal
 const MODAL = document.querySelector('#modal')
-const CLOSE_BUTTON = MODAL.querySelector('.modalCloseButton')
-const MODAL_CONTENT = MODAL.querySelector('.modalContent')
-const MODAL_TITLE = MODAL.querySelector('.modal_title')
+if(MODAL){
 
-CLOSE_BUTTON.addEventListener('click', () => {
-    MODAL.classList.remove('show')
-})
-const static_modal = (target_component, title, content) => {
-    const target = document.querySelectorAll(target_component)
+    const CLOSE_BUTTON = MODAL.querySelector('.modalCloseButton')
+    const MODAL_CONTENT = MODAL.querySelector('.modalContent')
+    const MODAL_TITLE = MODAL.querySelector('.modal_title')
 
-    const TARGETS = document.querySelectorAll(target_component)
-    TARGETS.forEach(target => target.addEventListener('click', ()=>{
-        MODAL.classList.toggle('show')
-        MODAL_CONTENT.innerText = content
-        MODAL_TITLE.innerText = title
-    }))
+    CLOSE_BUTTON.addEventListener('click', () => {
+        MODAL.classList.remove('show')
+    })
+    const static_modal = (target_component, title, content) => {
+        const target = document.querySelectorAll(target_component)
+    
+        const TARGETS = document.querySelectorAll(target_component)
+        TARGETS.forEach(target => target.addEventListener('click', ()=>{
+            MODAL.classList.toggle('show')
+            MODAL_CONTENT.innerText = content
+            MODAL_TITLE.innerText = title
+        }))
+    }
+    // empty item
+    static_modal('.emptyItemList_title', 'Item vazio!', 'Este item não possui conteúdo algum para ser visualizado no momento.')
 }
-// empty item
-static_modal('.emptyItemList_title', 'Item vazio!', 'Este item não possui conteúdo algum para ser visualizado no momento.')
