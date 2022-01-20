822c30464fdd942f2f1be52fdef9c9a9638c63aa
Kevin <kevin13348@gmail.com>
2022-01-20 10:49:07 -0300

    fixed issue: main menu in normal user's profile

--- public/js/modules/profile/main_menu.js
+++ public/js/modules/profile/main_menu.js
@@ -1,26 +1,29 @@
 import { get, on_click } from "../../../lib/main.js"
 import { this_user } from "../this_user.js"
 
-export default function init_menu(){
-    const MENU = get('#main_menu')
-    if(this_user.adm != 'true'){MENU.remove()}
+const USER_HAS_PERMISSION = this_user.adm === 'true'
+
+const MENU = get('#main_menu')
+
+function activate_buttons(){
+    const radios = [
+        {
+            button: MENU.querySelector('#radio_user_list'),
+            content: MENU.querySelector('#user_list_container')
+        },{
+            button: MENU.querySelector('#radio_approve_list'),
+            content: MENU.querySelector('#profile #requisition_list_container')
+        },
+    ]
     
-    if(MENU){
-        const radios = [
-            {
-               button: get('#radio_user_list'),
-               content: get('#user_list_container')
-            },{
-               button: get('#radio_approve_list'),
-               content: get('#profile #requisition_list_container')
-            },
-        ]
-        
+    function on_click_buttons(){
+
         radios.forEach(radio => {
             on_click(radio.button, ()=>{
                 const bg_img = get('#bg_img')
                 bg_img.classList.add('hide')
                 radios.forEach(radio => {const {button, content} = radio
+
                     if(button.checked){
                         content.classList.remove('hide')
                     }else{
@@ -30,4 +33,13 @@ export default function init_menu(){
             })
         })
     }
+    on_click_buttons()
+}
+
+export default function init_menu(){
+
+    if(USER_HAS_PERMISSION)
+        activate_buttons()
+    else
+        MENU.remove()
 }
