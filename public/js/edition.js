1ed273706b35c4080377a529d51887e37ebc0386
Kevin <kevin13348@gmail.com>
2022-01-20 11:07:48 -0300

    update: modularization -> edition.js, delete.js

--- public/js/edition.js
+++ public/js/edition.js
@@ -1,5 +1,7 @@
 import edition_mode from "./edition/open_edition.js";
 import update_mode from './edition/update_mode.js'
+import delete_mode from './edition/delete.js'
 
 edition_mode()
 update_mode()
+delete_mode()
--- public/js/edition/delete.js
+++ public/js/edition/delete.js
@@ -1,15 +1,28 @@
-import {get, on_click} from '/lib/main.js'
+import {get} from '/lib/main.js'
 import {this_user} from '/js/modules/this_user.js'
 
 const MAIN_BUTTON = get('#main_delete_button')
 const TRASH_BUTTONS = get('.delete_button_container')
 
-if(this_user.adm){
+function activate_buttons(){
+    const required_elements = [
+        MAIN_BUTTON,
+        TRASH_BUTTONS.length,
+    ]
+    const requirements_met = required_elements.every(element => element)
 
-    on_click(MAIN_BUTTON, ()=>{
+    function activate(){
         MAIN_BUTTON.classList.toggle('active')
         TRASH_BUTTONS.forEach(button => button.classList.toggle('hide'))
-    })
-}else{
-    MAIN_BUTTON.classList.add('hide')
+    }
+
+    requirements_met ? activate() : false
+}
+
+export default function init_delete_mode(){
+
+    if(this_user.adm === 'true')
+        MAIN_BUTTON.addEventListener('click', activate_buttons)
+    else
+        MAIN_BUTTON.remove()
 }
