edffbcb6e8fee782084287cf7cf3225060ad89ac
Kevin <kevin13348@gmail.com>
2022-01-21 11:16:54 -0300

    update: reseting user list filters

--- public/js/modules/profile/user_filter.js
+++ public/js/modules/profile/user_filter.js
@@ -86,6 +86,7 @@ function show_filter(select_value, filter_array){
 
 function reset_filters(filters){
     filters.forEach(filter => filter.value = '')
+    USER_COMPONENTS.forEach(user => show(user))
 }
 
 function filter_filters(){
