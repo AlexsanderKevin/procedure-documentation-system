53e9594f9d1a3c036f61a9053866d1ae2f3e67ea
Kevin <kevin13348@gmail.com>
2022-03-02 12:45:20 -0300

    fixed issue: require adm permission to add departments

--- public/js/modules/profile/add_department.js
+++ public/js/modules/profile/add_department.js
@@ -1,12 +1,13 @@
 import {get, on_click} from '/lib/main.js'
+import BOOLEAN_PERMISSION from '../../../lib/parse_boolean.js'
 
 export default function init_addition(){
+    const USER_HAS_PERMISSION = BOOLEAN_PERMISSION('adm')
     const ADD_BUTTON = get('#add_department_button')
     const ADD_FORM = get('#add_department_form')
     const CLOSE_BUTTON = get('#close_add_department_form')
     
-    if(ADD_BUTTON && ADD_FORM && CLOSE_BUTTON){
-    
+    function initButtons(){
         on_click(ADD_BUTTON, ()=>{
             ADD_FORM.classList.toggle('hide')
             ADD_BUTTON.classList.toggle('active')
@@ -16,4 +17,9 @@ export default function init_addition(){
             ADD_BUTTON.classList.remove('active')
         })
     }
+
+    if(USER_HAS_PERMISSION)
+        initButtons()
+    else
+        ADD_BUTTON.remove()
 }
