d9f4b3c7f14ad137cdd3b69419355c3532213e1f
Kevin <kevin13348@gmail.com>
2022-03-18 09:35:36 -0300

    refactor open_edition file in ESLint standards

--- public/js/modules/edition/open_edition.js
+++ public/js/modules/edition/open_edition.js
@@ -1,17 +1,16 @@
-import {get, on_click, close, open, on_change} from '../../../lib/main.js'
-import BOOLEAN_PERMISSION from '../../../lib/parse_boolean.js'
+import { get, on_click, close, open, on_change } from '../../../lib/main.js';
+import BOOLEAN_PERMISSION from '../../../lib/parse_boolean.js';
 
-const IS_USER_ADM = BOOLEAN_PERMISSION('adm')
-const IS_USER_EDITOR = BOOLEAN_PERMISSION('editor')
+const IS_USER_EDITOR = BOOLEAN_PERMISSION('editor');
 
-const bg_img = get('#edit_bg_img')
-const start_edition_button = get('#edition_mode_button')
+const bgImg = get('#edit_bg_img');
+const startEditionButton = get('#edition_mode_button');
 
-const modal = get('#edition_modal')
-const cancel_buttons = get('.cancel_button')
-const header = get('#edition_header')
-const add_options = get('#add_options_container')
-const add_forms = {
+const modal = get('#edition_modal');
+const cancelButtons = get('.cancel_button');
+const header = get('#edition_header');
+const addOptions = get('#add_options_container');
+const addForms = {
     add_department: get('#add_department_form'),
     add_procedure: get('#add_procedure_form'),
     add_section: get('#add_section_form'),
@@ -19,97 +18,99 @@ const add_forms = {
     add_solution: get('#add_solution_form'),
     add_issue: get('#add_issue_form'),
     add_obs: get('#add_obs_form'),
-}
-const UPDATE_BUTTONS = get('.edit_mode')
-const DELETE_BUTTONS = get('.delete_button_container')
-const MAIN_UPDATE_BUTTON = get('#main_edit_button')
+};
+const UPDATE_BUTTONS = get('.edit_mode');
+const DELETE_BUTTONS = get('.delete_button_container');
+const MAIN_UPDATE_BUTTON = get('#main_edit_button');
 
-function open_respective_modal(){
-    const required_elements = [
-        add_options,
-        add_forms,
+function openRespectiveModal() {
+    const requiredElements = [
+        addOptions,
+        addForms,
         modal,
-    ]
-    const requirements_met = required_elements.every(element => element)
+    ];
+    const requirementsMet = requiredElements.every((element) => element);
 
-    function selecting_modal(){
-        on_change(add_options, ()=>{
-            open(modal)
-            switch(add_options.value){
-                case 'department': open(add_forms.add_department)
-                    break
-                case 'procedure': open(add_forms.add_procedure)
-                    break
-                case 'section': open(add_forms.add_section)
-                    break
-                case 'subsection': open(add_forms.add_subsection)
-                    break
-                case 'solution': open(add_forms.add_solution)
-                    break
-                case 'issue': open(add_forms.add_issue)
-                    break
-                case 'obs': open(add_forms.add_obs)
-                    break
+    function selectModal() {
+        on_change(addOptions, () => {
+            open(modal);
+            switch (addOptions.value) {
+                case 'department': open(addForms.add_department);
+                    break;
+                case 'procedure': open(addForms.add_procedure);
+                    break;
+                case 'section': open(addForms.add_section);
+                    break;
+                case 'subsection': open(addForms.add_subsection);
+                    break;
+                case 'solution': open(addForms.add_solution);
+                    break;
+                case 'issue': open(addForms.add_issue);
+                    break;
+                case 'obs': open(addForms.add_obs);
+                    break;
+                default: break;
             }
-            add_options.value = ''
-        })
+            addOptions.value = '';
+        });
     }
-    requirements_met ? selecting_modal() : false
+
+    if (requirementsMet) selectModal();
 }
 
-function init_edition_mode(){
-    const required_elements = [
-        start_edition_button,
-        bg_img,
+function initEditionMode() {
+    const requiredElements = [
+        startEditionButton,
+        bgImg,
         header,
         UPDATE_BUTTONS,
         DELETE_BUTTONS,
         MAIN_UPDATE_BUTTON,
-    ]
-    const requirements_met = required_elements.every(element => element)
+    ];
+    const requirementsMet = requiredElements.every((element) => element);
 
-    function click_to_start(){
-        on_click(start_edition_button, ()=>{
-            bg_img.classList.toggle('hide')
-            header.classList.toggle('hide')
-            UPDATE_BUTTONS.forEach(button => button.classList.add('hide'))
-            DELETE_BUTTONS.forEach(button => button.classList.add('hide'))
-            MAIN_UPDATE_BUTTON.classList.remove('active')
-        })
-    }
-    requirements_met ? click_to_start() : false
-}
-function close_add_modal(){
-    const required_elements = [
-        cancel_buttons,
-        modal, 
-        add_forms,
-    ]
-    const requirements_met = required_elements.every(element => element)
-
-    function close_all_modals(){
-        cancel_buttons.forEach(button => {
-            on_click(button, ()=>{
-                close(modal)
-                close(add_forms.add_procedure)
-                close(add_forms.add_section)
-                close(add_forms.add_subsection)
-                close(add_forms.add_solution)
-                close(add_forms.add_obs)
-                close(add_forms.add_issue)
-            })
+    function clickToStart() {
+        on_click(startEditionButton, () => {
+            bgImg.classList.toggle('hide');
+            header.classList.toggle('hide');
+            UPDATE_BUTTONS.forEach((button) => button.classList.add('hide'));
+            DELETE_BUTTONS.forEach((button) => button.classList.add('hide'));
+            MAIN_UPDATE_BUTTON.classList.remove('active');
         });
     }
-    requirements_met ? close_all_modals() : false
+    if (requirementsMet) clickToStart();
 }
 
-export default function start_edition(){
-    if(IS_USER_EDITOR){
-        init_edition_mode()
-        close_add_modal()
-        open_respective_modal()
-        
-    }else{
-        start_edition_button ? start_edition_button.remove() : false
+function closeAddModal() {
+    const requiredElements = [
+        cancelButtons,
+        modal,
+        addForms,
+    ];
+    const requirementsMet = requiredElements.every((element) => element);
+
+    function closeAllModals() {
+        cancelButtons.forEach((button) => {
+            on_click(button, () => {
+                close(modal);
+                close(addForms.add_procedure);
+                close(addForms.add_section);
+                close(addForms.add_subsection);
+                close(addForms.add_solution);
+                close(addForms.add_obs);
+                close(addForms.add_issue);
+            });
+        });
+    }
+    if (requirementsMet) closeAllModals();
+}
+
+export default function startEdition() {
+    if (IS_USER_EDITOR) {
+        initEditionMode();
+        closeAddModal();
+        openRespectiveModal();
+    } else {
+        startEditionButton.remove();
     }
 }
