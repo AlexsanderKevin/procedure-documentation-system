757925ba8a2713880bfec5c1a8c9cbfe21d0afac
Kevin <kevin13348@gmail.com>
2022-03-21 09:06:21 -0300

    deleted login validation front features

--- public/js/login.js
+++
@@ -1,3 +0,0 @@
-import form_validation from './modules/login/form_validation.js';
-
-form_validation();
--- public/js/modules/login/form_validation.js
+++
@@ -1,8 +0,0 @@
-export default function login_form_validation(){
-    
-    const form = document.forms.login_form
-    document.addEventListener('keyup', handleKeyup)
-    
-    function handleKeyup(event){
-    }
-}
